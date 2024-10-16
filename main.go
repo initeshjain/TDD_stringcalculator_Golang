f46e35ceeba5a93e806196cb557e046d9eee7d19
initeshjain <nitesh.exe@gmail.com>
2024-10-17 00:57:11 +0530

    Refactor: Extract custom delimiter logic into separate function

--- main.go
+++ main.go
@@ -6,36 +6,39 @@ import (
 	"strings"
 )
 
+// Add takes a string of numbers and returns their sum.
 func Add(numbers string) int {
-    if numbers == "" {
-        return 0
-    }
+	if numbers == "" {
+		return 0
+	}
 
-    delimiter := ","
-    if strings.HasPrefix(numbers, "//") {
-        parts := strings.SplitN(numbers, "\n", 2)
-        delimiter = parts[0][2:]
-        numbers = parts[1]
-    }
+	delimiter, remaining := extractDelimiter(numbers)
+	remaining = strings.ReplaceAll(remaining, "\n", delimiter)
+	parts := strings.Split(remaining, delimiter)
 
-    numbers = strings.ReplaceAll(numbers, "\n", delimiter)
-    parts := strings.Split(numbers, delimiter)
+	sum := 0
+	negatives := []int{}
+	for _, part := range parts {
+		num, _ := strconv.Atoi(part)
+		if num < 0 {
+			negatives = append(negatives, num)
+		}
+		if num <= 1000 {
+			sum += num
+		}
+	}
 
-    sum := 0
-    negatives := []int{}
-    for _, part := range parts {
-        num, _ := strconv.Atoi(part)
-        if num < 0 {
-            negatives = append(negatives, num)
-        }
+	if len(negatives) > 0 {
+		panic(fmt.Sprintf("negatives not allowed: %v", negatives))
+	}
+	return sum
+}
 
-        if num <= 1000 {
-  		  	sum += num
-				}
-    }
-
-    if len(negatives) > 0 {
-        panic(fmt.Sprintf("negatives not allowed: %v", negatives))
-    }
-    return sum
+// extractDelimiter handles custom delimiter logic.
+func extractDelimiter(input string) (string, string) {
+	if strings.HasPrefix(input, "//") {
+		parts := strings.SplitN(input, "\n", 2)
+		return parts[0][2:], parts[1]
+	}
+	return ",", input
 }
